e91615aad8680cc831b55c5f5f538cefc2c49d37
AliHassanL12 <liaqqahl@gmail.com>
2024-09-25 23:53:27 +0100

    Create 16x16 grid
    This is where the player will play the 'Etch-a-sketch' game.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,12 @@
-console.log('hey');
+const container = document.querySelector('.container')
+
+for (let i = 1; i <= 16; i++) {
+    const gridContainer = document.createElement('div');
+    gridContainer.setAttribute('class', 'gridContainer');
+    container.appendChild(gridContainer);
+    for (let j = 1; j <= 16; j++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'gridSquare');
+        gridContainer.appendChild(div);
+    }
+}
