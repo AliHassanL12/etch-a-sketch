da613349534f83614edd585725c091eab82d3a4c
AliHassanL12 <liaqqahl@gmail.com>
2024-09-26 05:42:58 +0100

    Add logic for resetting grid
    This allows us to choose a new grid up to 100, and increase the n-
    umber of squares per side while keeping the canvas the same size.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,20 +1,42 @@
 const container = document.querySelector('.container')
 
-for (let i = 1; i <= 16; i++) {
-    const gridContainer = document.createElement('div');
-    gridContainer.setAttribute('class', 'gridContainer');
-    container.appendChild(gridContainer);
-    for (let j = 1; j <= 16; j++) {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'gridSquare');
-        gridContainer.appendChild(div);
+
+function createGrid(numberOfSqrs = 16) {
+    for (let i = 1; i <= numberOfSqrs; i++) {
+        const gridContainer = document.createElement('div');
+        gridContainer.setAttribute('class', 'gridContainer');
+        container.appendChild(gridContainer);
+        for (let j = 1; j <= numberOfSqrs; j++) {
+            const div = document.createElement('div');
+            div.setAttribute('class', 'gridSquare');
+            gridContainer.appendChild(div);
+        }
     }
+
+    const squares = document.querySelectorAll('.gridSquare');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hoveredSquare');
+        })
+    })
 }
 
-const squares = document.querySelectorAll('.gridSquare');
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('hoveredSquare');
+function deleteGrid() {
+    const gridContainers = document.querySelectorAll('.gridContainer');
+    gridContainers.forEach((gridContainer) => {
+        gridContainer.remove();
     })
+}
+
+const resetButton = document.querySelector('.resetButton');
+
+resetButton.addEventListener('click', () => {
+    const getNewGridSize = +prompt('How many squares would you like per side?');
+    if (getNewGridSize <= 100) {
+        deleteGrid();
+        createGrid(getNewGridSize);
+    }
 })
+
+createGrid();
